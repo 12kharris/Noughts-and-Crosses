7350bf64859609d5f2ae81bcfa02ed3cd799d688
12kharris Harris <kurtharris1541@gmail.com>
2024-02-20 17:56:06 +0000

    Add function to check if a player has won

--- run.py
+++ run.py
@@ -61,6 +61,45 @@ def validate_and_place_counter(marker_pos, board, player_symbol, player_1):
         print("Please enter a whole number on the board where you would like to place your marker")
         return False
 
+def check_winner(board, board_size):
+    #credit to https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python
+    #for how to get unique items in a list
+
+    #horizontal check   
+    for i in range(board_size):
+        horizontal_values = []
+        for j in range(board_size):
+            horizontal_values.append(board[i][j])
+        horizontal_unq = set(horizontal_values)
+        if (len(horizontal_unq) == 1): # a set contains unique values. So if there is only 1 value, the line is complete
+            return True
+
+    #vertical check    
+    for i in range(board_size):
+        vertical_values = []
+        for j in range(board_size):
+            vertical_values.append(board[j][i])
+        vertical_unq = set(vertical_values)
+        if (len(vertical_unq) == 1):
+            return True
+
+    #diagonal check top L to bottom R
+    diagonal_LR_values = []
+    for i in range(board_size):
+        diagonal_LR_values.append(board[i][i])
+    diagonal_LR_unq = set(diagonal_LR_values)
+    if(len(diagonal_LR_unq) == 1):
+        return True
+
+    #diagonal check top R to bottom L
+    diagonal_RL_values = []
+    for i in range(board_size):
+        diagonal_RL_values.append(board[i][board_size - 1 - i])
+    diagonal_RL_unq = set(diagonal_RL_values)
+    if(len(diagonal_RL_unq) == 1):
+        return True
+
+    return False
 
 def main():
 
@@ -70,13 +109,13 @@ def main():
         board_input = input("Please enter how large you want the board to be (minimum 3, maximum 9) - ")
         valid_board = validate_board_size(board_input)
     board = Board(int(board_input))  
+    draw_board(board.board, board.board_size)
 
     player = True
     counters_placed = 0
 
     #While there are still unnoccupied spaces on the board
     while (counters_placed < board.board_size**2):
-        draw_board(board.board, board.board_size)
         player_symbol = ""
         valid_placement = False
         
@@ -90,7 +129,16 @@ def main():
             while (not valid_placement):
                 marker_pos = rand.randint(0, board.board_size**2 - 1)
                 valid_placement = validate_and_place_counter(marker_pos, board, player_symbol, player)
+            draw_board(board.board, board.board_size)
             print(f"\nComputer chose - {marker_pos}")
+            
+        if(check_winner(board.board, board.board_size)):
+            draw_board(board.board, board.board_size)
+            if (player):
+                print("\nCongratulations! You win!")
+            else:
+                print("\nComputer wins!")
+            return
         
         player = not player
         counters_placed += 1
